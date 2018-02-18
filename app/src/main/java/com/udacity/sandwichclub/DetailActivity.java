10675a744a249a5ec74afc149112b4fcf6139e8d
Mustafa Berkay Mutlu <mustafaberkaymutlu@gmail.com>
2018-02-18 15:10:18 +0300

    Implement displaying detail info

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -1,9 +1,13 @@
 package com.udacity.sandwichclub;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -12,43 +16,42 @@ import com.udacity.sandwichclub.utils.JsonUtils;
 
 public class DetailActivity extends AppCompatActivity {
 
-    public static final String EXTRA_POSITION = "extra_position";
+    private static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+
+    public static Intent newIntent(@NonNull Context context, int position) {
+        final Intent intent = new Intent(context, DetailActivity.class);
+        intent.putExtra(EXTRA_POSITION, position);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
-
-        Intent intent = getIntent();
+        final Intent intent = getIntent();
         if (intent == null) {
             closeOnError();
         }
 
-        int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
+        final int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
         if (position == DEFAULT_POSITION) {
             // EXTRA_POSITION not found in intent
             closeOnError();
             return;
         }
 
-        String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
-        String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+        final String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
+        final String json = sandwiches[position];
+        final Sandwich sandwich = JsonUtils.parseSandwichJson(json);
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
             return;
         }
 
-        populateUI();
-        Picasso.with(this)
-                .load(sandwich.getImage())
-                .into(ingredientsIv);
-
-        setTitle(sandwich.getMainName());
+        populateUI(sandwich);
     }
 
     private void closeOnError() {
@@ -56,7 +59,24 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(@NonNull Sandwich sandwich) {
+        final ImageView ivIngredients = findViewById(R.id.image_iv);
+        final TextView tvOrigin = findViewById(R.id.origin_tv);
+        final TextView tvAlsoKnownAs = findViewById(R.id.also_known_tv);
+        final TextView tvDescription = findViewById(R.id.description_tv);
+        final TextView tvIngredients = findViewById(R.id.ingredients_tv);
 
+        if (!TextUtils.isEmpty(sandwich.getImage())) {
+            Picasso.with(this)
+                    .load(sandwich.getImage())
+                    .into(ivIngredients);
+        }
+
+        tvOrigin.setText(sandwich.getPlaceOfOrigin());
+        tvAlsoKnownAs.setText(TextUtils.join(", ", sandwich.getAlsoKnownAs()));
+        tvDescription.setText(sandwich.getDescription());
+        tvIngredients.setText(TextUtils.join(", ", sandwich.getIngredients()));
+
+        setTitle(sandwich.getMainName());
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/MainActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,12 +16,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] sandwiches = getResources().getStringArray(R.array.sandwich_names);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+        final String[] sandwiches = getResources().getStringArray(R.array.sandwich_names);
+        final ListAdapter adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, sandwiches);
 
         // Simplification: Using a ListView instead of a RecyclerView
-        ListView listView = findViewById(R.id.sandwiches_listview);
+        final ListView listView = findViewById(R.id.sandwiches_listview);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -31,8 +32,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void launchDetailActivity(int position) {
-        Intent intent = new Intent(this, DetailActivity.class);
-        intent.putExtra(DetailActivity.EXTRA_POSITION, position);
+        final Intent intent = DetailActivity.newIntent(this, position);
         startActivity(intent);
     }
 }
